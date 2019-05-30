896f50761511062a9ea34c0edc33cd85faa7c17c
Wei Cheng Hung <s777610@gmail.com>
2019-05-30 02:04:23 -0700

    update resolvers for car

--- resolvers/car.js
+++ resolvers/car.js
@@ -1,34 +1,28 @@
 const resolvers = {
   Query: {
-    car: (parent, { id }, { models }) => models.cars.find(car => car.id === id),
-    cars: (parent, args, { models }) => models.cars
+    car: (parent, { id }, { models }) => models.Car.findByPk(id),
+    cars: (parent, args, { models }) => models.Car.findAll()
   },
   Mutation: {
-    createCar: (parent, { id, make, model, color }, { models }) => {
+    createCar: (parent, { make, model, color }, { models }) => {
       const car = {
-        id,
         make,
         model,
         color
       };
-      models.cars.push(car);
-      return car;
+      return models.Car.create(car);
     },
     removeCar: (parent, { id }, { models }) => {
-      let found = false;
-      models.cars = models.cars.filter(car => {
-        if (car.id === id) found = true;
-        else return car;
+      return models.Car.destroy({
+        where: {
+          id
+        }
       });
-      if (found) return true;
-      else return false;
     }
   },
   Car: {
     ownedBy: (parent, args, { models }) => {
-      // parent is Car
-      // console.log(parent); { id: 2, make: 'Honda', model: 'HRV', color: 'red', ownedBy: 1 }
-      return models.users.find(user => user.id === parent.ownedBy);
+      return models.User.findByPk(parent.userId);
     }
   }
 };
